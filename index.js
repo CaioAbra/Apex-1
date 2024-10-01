1fdca730a776536a7e9eba941a2040cec7935c3c
Kleiton Tomazi <kleitontomaaazi@gmail.com>
2024-09-30 22:29:32 -0300

    finalizando login page e adicionando register

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const togglePassword = document.querySelector("#togglePassword");
-  const passwordField = document.querySelector("#password");
+  const togglePasswordVisibility = document.querySelector(
+    "#togglePasswordVisibility"
+  );
+  const userPasswordField = document.querySelector("#userPassword");
 
-  togglePassword.addEventListener("click", function () {
-    const type =
-      passwordField.getAttribute("type") === "password" ? "text" : "password";
-    passwordField.setAttribute("type", type);
+  const loginContainer = document.querySelector("#loginContainer");
+
+  const registerButton = document.querySelector("#registerButton");
+  const registerContainer = document.querySelector("#registerContainer");
+
+  togglePasswordVisibility.addEventListener("click", function () {
+    const passwordFieldType =
+      userPasswordField.getAttribute("type") === "password"
+        ? "text"
+        : "password";
+    userPasswordField.setAttribute("type", passwordFieldType);
 
     this.classList.toggle("bi-eye");
     this.classList.toggle("bi-eye-slash");
+  });
+
+  registerButton.addEventListener("click", function () {
+    loginContainer.style.display = "none";
+    registerContainer.style.display = "flex";
   });
 });
